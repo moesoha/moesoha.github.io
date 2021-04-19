e728db2e931a04159430aeff54dd19c79aef0bc6
Soha Jin <soha@lohu.info>
2021-04-20 00:02:47 +0800

    add google analytics

--- gulpfile.js
+++ gulpfile.js
@@ -76,6 +76,13 @@ const htmlCompile = () => src('./*.html')
 					}
 				}
 			}
+
+			const gaTrackId = (process.env.GOOGLE_ANALYTICS_ID || '').trim();
+			if (gaTrackId) {
+				const script = `<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create','${gaTrackId}','auto');ga('send','pageview');</script>`;
+				$(script).appendTo($('body'));
+			}
+
 			file.contents = Buffer.from($.html());
 			this.push(file);
 		}
