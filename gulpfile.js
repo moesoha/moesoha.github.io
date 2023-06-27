2af32dd15a8eab6cec01da84a18aec16162991cb
Soha Jin <soha@lohu.info>
2023-06-27 19:04:20 +0000

    use Google Analytics 4

--- gulpfile.js
+++ gulpfile.js
@@ -79,8 +79,8 @@ const htmlCompile = () => src('./*.html')
 
 			const gaTrackId = (process.env.GOOGLE_ANALYTICS_ID || '').trim();
 			if (gaTrackId) {
-				const script = `<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create','${gaTrackId}','auto');ga('send','pageview');</script>`;
-				$(script).appendTo($('body'));
+				$(`<script async src="https://www.googletagmanager.com/gtag/js?id=${gaTrackId}"></script>`).appendTo($('body'));
+				$(`<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${gaTrackId}');</script>`).appendTo($('body'));
 			}
 
 			file.contents = Buffer.from($.html());
